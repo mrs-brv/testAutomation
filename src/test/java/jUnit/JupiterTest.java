a3ee79eca96a49b4062f9cad5e7603840446e8d5
mrs-brv <szkolenie@brvsl.az.pl>
2018-01-31 09:10:09 +0100

    testy trzeciego dnia, pierwszy commit brancha

--- src/test/java/jUnit/JupiterTest.java
+++ src/test/java/jUnit/JupiterTest.java
@@ -4,6 +4,9 @@ package jUnit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.Assert.assertTrue;
 
@@ -15,5 +18,18 @@ public class JupiterTest extends ConfigJupiter{
     @RepeatedTest(5)
     public void jupiterTest(){
         assertTrue(testowy == 20);
+    }
+
+    @ParameterizedTest
+    @DisplayName("Testy z CSV")
+    @CsvFileSource(resources = "/plik.csv")
+    public void csvTest(String name, Integer age){
+        assertTrue(name instanceof String);
+        assertTrue(age instanceof Integer);
+    }
+    @ParameterizedTest
+    @ValueSource(ints={1,2,3,4})
+    public void withValueSourceTest(int value){
+        assertTrue(value<100);
     }
 }
