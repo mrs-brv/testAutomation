90b40a0829b6fefa4b2886df855191ad52bbcc33
mrs-brv <szkolenie@brvsl.az.pl>
2018-02-01 15:17:30 +0100

    testy trzeciego dnia, pierwszy commit mastera

---
+++ src/main/java/com/jsystems/models/MyObj.java
@@ -0,0 +1,19 @@
+package com.jsystems.models;
+
+public class MyObj {
+    public String name;
+    public String surname;
+
+    public MyObj(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return "MyObj{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+}
